607130da8b2afc2e5236c88c164be2b51b5962b3
Tharoth_Touch <touchtharoth@gmail.com>
2020-01-13 16:42:05 +0700

    GameDev HW Week8

--- Assets/Scripts/Defender.cs
+++ Assets/Scripts/Defender.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Defender : MonoBehaviour {
-
 	[SerializeField] int starCost = 100;
+	public void AddStars(int amount){
+		FindObjectOfType<StarDisplay>().AddStars(amount);
+	}
 	
 	 
 }
